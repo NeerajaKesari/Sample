960c10de4f7f05436c2acf919fc104f63b7b5883
KESARI NEERAJA <KESARI NEERAJA@LAPTOP-413PFH49>
2020-11-18 15:00:24 +0530

    edited by Neer

---
+++ neeraja/src/day3/MatrixMultiplication.java
@@ -0,0 +1,31 @@
+package day3;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+		int x[][]={{1,2,3},{1,2,3},{1,2,3}};
+		int y[][]={{1,2,3},{1,2,3},{1,2,3}};
+		int z[][]=new int[3][3];
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++) 
+			{
+				z[i][j]=0;    
+				for(int k=0;k<3;k++)    
+				{    
+				  z[i][j]+=x[i][k]*y[k][j];    
+				}
+				System.out.print(z[i][j]+" ");  
+			}
+				System.out.println();  
+		} 
+	}
+}
+				
+			
+		
+		
+
+	
+
+
